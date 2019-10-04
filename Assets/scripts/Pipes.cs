8c5eef5dc4b936cc687b99a8653f7670f9225fcd
Fernando Santana Rodrigues - 519112473 <519112473@animaedu.intranet>
2019-10-04 11:25:49 -0300

    Projeti

---
+++ Assets/scripts/Pipes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float speed = 1;
+
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }	
+}
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float _Timer;
+
+    public GameObject pipesPrefab;
+    public float height;
+    public float maxTime = 1f;
+
+    private void Start()
+    {
+        var newPipes = Instantiate(pipesPrefab);
+        var y = Random.Range(-height, height);
+
+        newPipes.transform.position = new Vector2(0, y);
+    }
+
+    private void Update()
+    {
+        if (_Timer > maxTime)
+        {
+            var newPipes = Instantiate(pipesPrefab);
+            var y = Random.Range(-height, height);
+
+            newPipes.transform.position = new Vector2(0, y);
+            Destroy(newPipes, 20f);
+
+            _Timer = 0;
+        }
+
+        _Timer += Time.deltaTime;
+    }
+
+
+ private void Spawn()
+ {
+    var newPipes = Instantiate(pipesPrefab);
+    var y = Random.Range(-height, height);
+
+    newPipes.transform.position = new Vector2(Transform.position.x, y);
+    Destroy(newPipes, 20f);
+ }
+}
+
